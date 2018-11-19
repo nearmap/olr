838d2a3fd4e268eff9db4923cc6e5aabe0332616
Anu Sridhar <anu.sridhar@nearmap.com>
2018-11-19 11:13:12 +1100

    feat(scripts): npx migration
    Migrated all scripts from npm run to npx. Created scripts/index.js to hold the necessary scripts.

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+export default {
+  start: 'echo TODO',
+
+  build: 'run-s build:*',
+  'build:pkg': "babel src --out-dir build/pkg --ignore '**/*.test.js'",
+  'build:files': (
+    'cp ./package.json build/pkg/ && ' +
+    'cp ./README.md build/pkg/ && ' +
+    'cp ./LICENSE build/pkg/'
+  ),
+  
+
+  clean: 'rimraf ./build',
+
+  commit: 'run git-cz',
+
+  test: 'run lint jest:full',
+
+  lint: 'run lint:*',
+  'lint:js': (
+    'eslint --report-unused-disable-directives --ignore-path .gitignore .'
+  ),
+  'lint:md': 'remark -i .gitignore --no-stdout --use remark-lint *.md',
+
+  jest: 'jest --collectCoverage=false --cache=true',
+  'jest:full': 'jest --verbose --runInBand --no-cache',
+
+  preci: 'npm --loglevel=warn install',
+  ci: 'run clean test build',
+  release: 'run -s clean build semantic-release',
+  'semantic-release': 'semantic-release'
+};
